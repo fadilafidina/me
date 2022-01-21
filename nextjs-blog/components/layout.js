a58be53177194a187808b1dc1735d1e0a89cbf2d
Fadila Fidina <fadila_fidina@hotmail.com>
2022-01-22 07:43:26 +1100

    WHY THE FLIP DO YOU JITTER WHEN I CHANGE PAGES ID ONT GE T IT KLHBUHPOHI;IKH

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,40 +5,22 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Navbar from './navbar'
 
-const name = 'voldy'
 export const siteTitle = 'howdy 🤠'
 
 export default function Layout({ children, home }) {
   return (
-    <div className='place-items-center h-screen pt-6'>
+    <div className='pt-12'>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="me struggling to build something haha okay"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Navbar></Navbar>
 
-      <div className="pt-6 align-middle container mx-auto min-h-0">
+      <div className="p-12 grid justify-center">
       <main>{children}</main>
-      
-      
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
       </div>
     </div>
   )
--- nextjs-blog/components/navbar.jsx
+++ nextjs-blog/components/navbar.jsx
@@ -1,40 +1,38 @@
-import React from "react";
 import Link from "next/link";
+import React, { useState } from "react";
 
 export default function Navbar() {
     return (
-        <div>
-            <div className="font-mono flex flex-row justify-items-center content-center text-center">
-                <div className="basis-1/4 text-indigo-600">
-                    <Link href="/">
-                        <a>
-                            fadila fidina
-                        </a>
-                    </Link>
-                </div>
-                <div className="basis-1/4">
-                    <Link href="/about">
-                        <a>
-                            about
-                        </a>
-                    </Link>
-                </div>
-                <div className="basis-1/4">
-                    <Link href="/projects">
-                        <a>
-                            projects
-                        </a>
-                    </Link>
-                </div>
+            <div className="flex items-center justify-center">
 
-                <div className="basis-1/4">
-                    <Link href="/creative-stuff">
-                        <a>
-                            creative stuff
-                        </a>
-                    </Link>
+                    <div className="px-8 hover:text-indigo-800 w-30">
+                        <Link href="/">
+                            <a>
+                                ffs
+                            </a>
+                        </Link>
+                    </div>
+                        <div className="px-8 hover:text-indigo-800 w-30">
+                            <Link href="/about">
+                                <a>
+                                    about
+                                </a>
+                            </Link>
+                        </div>
+                        <div className="px-8 hover:text-indigo-800 w-30">
+                            <Link href="/projects">
+                                <a>
+                                    projects
+                                </a>
+                            </Link>
+                        </div>
+                        <div className="px-8 hover:text-indigo-800 w-30">
+                            <Link href="/creative-stuff">
+                                <a>
+                                    other stuff
+                                </a>
+                            </Link>
+                        </div>
                 </div>
-            </div>
-        </div>
-    )
+)
 }
