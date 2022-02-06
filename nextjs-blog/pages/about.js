26f7c67ae15d796a4d14ef0d1e18b78ca5c4c592
Fadila Fidina <fadila_fidina@hotmail.com>
2022-02-07 10:23:18 +1100

    add details to about

--- nextjs-blog/pages/about.js
+++ nextjs-blog/pages/about.js
@@ -13,7 +13,32 @@ export default function FirstPost() {
     </h1>
 
     <div className='pt-4'>
-        Insert some lorem ipsum. I really like pizza. Please work.
+        My name is Fadila Fidina.
     </div>
+
+    <div className='pt-4'>
+        I am a full-stack software engineer, and I love creating and building all sorts of things.
+    </div>
+
+    <div className='pt-4'>
+        From software, to things that are edible, to websites, to paintings, I love to bring things to life and to share them with the world around me.
+    </div>
+
+    <div className='pt-4'>
+        The stack that I've used with include .NET / C#, React + Redux, JavaScript, TypeScript, GraphQL.
+    </div>
+
+    <div className='pt-4'>
+        Stores I've played with include SQL, PostgreSQL, MongoDB and Elasticsearch.
+    </div>
+
+    <div className='pt-4'>
+        I built this site using Next.js, TailwindsCSS, React and deployed it using Vercel.
+    </div>
+
+    <div className='pt-4 flex'>
+        Aside from code, I love to bake bread, cakes, loafs, buns, pizzas, and anything that could go into an oven! I also like to draw and paint!
+    </div>
+
     </Layout>
 }
