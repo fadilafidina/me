0b02da8d2331d305509fe9f776a0b31f4193ab91
Fadila <fadila_fidina@hotmail.com>
2022-08-17 19:32:59 +1000

    paddding on phones. next i need to fix the toggle bc links dead pn pjone

--- nextjs-blog/components/Navbar.jsx
+++ nextjs-blog/components/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
     if (useNewNavBar) {
         return (
 
-            <nav class='bg-slate-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800'>
-                <div class='container flex flex-wrap justify-between items-center mx-auto'>
+            <nav class='bg-slate-50 border-gray-200 px-1 sm:px-8 py-4 rounded dark:bg-gray-800'>
+                <div class='container sm:px-0 px-8 flex flex-wrap justify-between items-center mx-auto'>
                     <Link href='/' class='flex items-center'>
                         <span class='self-center text-base font-semibold whitespace-nowrap dark:text-white'>Fadila Fidina 💫</span>
                     </Link>
